98990f470e91495c6ab1c818f6a919d119059c47
micah5 <98mprice@gmail.com>
2023-06-04 16:14:52 +0200

    add some documention to the cube

--- examples/cube.go
+++ examples/cube.go
@@ -6,7 +6,21 @@ import (
 
 func main() {
 	w, d, h := 1.0, 1.0, 1.0
-	node := toothpaste.NewNode(toothpaste.Square(w, d).To3D())
+
+	// Create a 2D shape that can be extruded
+	// There are some built-in shapes, but you can also create your own
+	square := toothpaste.Square(w, d)
+
+	// A node is a face that is connected to other faces
+	// Let's create a node from the square (at z=0)
+	node := toothpaste.NewNode(square.To3D())
+
+	// Extrude the node to create a cube
 	node.Extrude(h)
+
+	// Center the cube at the origin
+	node.Center()
+
+	// Generate the .obj file
 	node.Generate("cube.obj")
 }
