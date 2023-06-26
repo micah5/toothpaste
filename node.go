949955115aebc5543ea390a8b60a19f9fd2984ee
micah5 <98mprice@gmail.com>
2023-06-26 15:55:30 +0800

    add snap for nodes

--- node.go
+++ node.go
@@ -694,6 +694,28 @@ func (ns Nodes) Translate2D(x, y float64) {
 	}
 }
 
+func (ns Nodes) Snap(point *Vertex3D) {
+	// find closest vertex
+	var closest *Vertex3D
+	var closestDist float64
+	faces := ns[0].Faces()
+	for _, f := range faces {
+		for _, vertex := range f.Vertices {
+			dist := vertex.Distance(point)
+			if closest == nil || dist < closestDist {
+				closest = vertex
+				closestDist = dist
+			}
+		}
+	}
+
+	// snap all to closest vertex
+	uniques := ns.UniqueVertices()
+	for _, v := range uniques {
+		v.Translate(point.X-closest.X, point.Y-closest.Y, point.Z-closest.Z)
+	}
+}
+
 func (ns Nodes) Get(tag string) Nodes {
 	return ns[0].GetAll(tag)
 }
